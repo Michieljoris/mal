a9fc2b53848abe0a66c0153bbbdf8e5a887c2f9f
Michiel van Oosten <mail@axion5.net>
2015-08-03 12:43:09 +1000

    Update

--- js/special.js
+++ js/special.js
@@ -6,47 +6,47 @@ MAL.special = (function(env) {
   function special(fn) {
     fn.special = true;
     return fn;
-    };
+  };
 
-    return {
-      "def!": special(function(env, args) {
-        return { ast: env[args[0].toString()] = EVAL(args[1], env) };
-      }),
-      "let*": special(function(env, args) {
-        var newEnv = Object.create(env);
-        var bindings = args[0];
-        while (bindings.length) {
-          //TODO: check for even, symbol/value pairs..
-          newEnv[bindings.shift().toString()] = EVAL(bindings.shift(), newEnv);
-        }
-        return { ast: args[1] || null, env: newEnv };
-      }),
-      "do": special(function(env, args) {
-        args.slice(0, args.length-1).forEach(function(ast) {
-          EVAL(ast, env);
-        });
-        var last = args.slice(args.length-1);
-          return { ast: last[0] || null};
-      }),
-      "if": special(function(env, args) {
-        var cond = EVAL(args[0], env);
-        if (cond !== null && cond !== false) return { ast: args[1] };
-        else return { ast: args[2] || null};
-      }),
-      "fn*": special(function(env, args) {
-        var params = args[0];
-        var body = args[1];
-        var fn = function(expressions) {
-          return EVAL(body, bind(env, params, expressions));
-        };
-        return {
-          ast: body,
-          env: env,
-          fn: fn,
-          params: params
-        };
-      })
-    };
+  return {
+    "def!": special(function(env, args) {
+      return { ast: env[args[0].toString()] = EVAL(args[1], env) };
+    }),
+    "let*": special(function(env, args) {
+      var newEnv = Object.create(env);
+      var bindings = args[0];
+      while (bindings.length) {
+        //TODO: check for even, symbol/value pairs..
+        newEnv[bindings.shift().toString()] = EVAL(bindings.shift(), newEnv);
+      }
+      return { ast: args[1] || null, env: newEnv };
+    }),
+    "do": special(function(env, args) {
+      args.slice(0, args.length-1).forEach(function(ast) {
+        EVAL(ast, env);
+      });
+      var last = args.slice(args.length-1);
+      return { ast: last[0] || null};
+    }),
+    "if": special(function(env, args) {
+      var cond = EVAL(args[0], env);
+      if (cond !== null && cond !== false) return { ast: args[1] };
+      else return { ast: args[2] || null};
+    }),
+    "fn*": special(function(env, args) {
+      var params = args[0];
+      var body = args[1];
+      var fn = function(expressions) {
+        return EVAL(body, bind(env, params, expressions));
+      };
+      return {
+        ast: body,
+        env: env,
+        fn: fn,
+        params: params
+      };
+    })
+  };
 
 })((function() {
   var inNode = typeof module !== 'undefined';
